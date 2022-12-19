f46d05a1b77a8c72bdad8476a7f0f8d7e8480b98
Pedro Calado <pedrocalado22@gmail.com>
2022-12-19 08:17:53 -0300

    Moves ViewCode protocol methods to an extension in the AbilityDescriptionView

--- Pokedex/Views/AbilityDescriptionView.swift
+++ Pokedex/Views/AbilityDescriptionView.swift
@@ -9,35 +9,8 @@ import Foundation
 import UIKit
 import RxSwift
 
-class AbilityDescriptionViewController: UIViewController, ViewCode {
+class AbilityDescriptionViewController: UIViewController {
     let disposeBag = DisposeBag()
-    
-    func buildHierarchy() {
-        view.addSubview(containerView)
-        containerView.addSubview(abilityDescriptionTitle)
-        containerView.addSubview(abilityDescription)
-    }
-    
-    func setupConstraints() {
-        NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            containerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-
-            abilityDescriptionTitle.topAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.topAnchor, constant: 20),
-            abilityDescriptionTitle.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
-            abilityDescriptionTitle.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
-            
-            abilityDescription.topAnchor.constraint(equalTo: abilityDescriptionTitle.topAnchor, constant: 50),
-            abilityDescription.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            abilityDescription.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-        ])
-    }
-    
-    func applyAdditionalChanges() {
-        view.backgroundColor = .white
-    }
     
     private var viewModel: AbilityDescriptionViewModelProtocol?
     
@@ -116,5 +89,34 @@ extension AbilityDescriptionViewController {
         
         alert.addAction(okAction)
         self.present(alert, animated: true)
+    }
+}
+
+extension AbilityDescriptionViewController: ViewCode {
+    func buildHierarchy() {
+        view.addSubview(containerView)
+        containerView.addSubview(abilityDescriptionTitle)
+        containerView.addSubview(abilityDescription)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+
+            abilityDescriptionTitle.topAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.topAnchor, constant: 20),
+            abilityDescriptionTitle.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
+            abilityDescriptionTitle.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
+            
+            abilityDescription.topAnchor.constraint(equalTo: abilityDescriptionTitle.topAnchor, constant: 50),
+            abilityDescription.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            abilityDescription.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    func applyAdditionalChanges() {
+        view.backgroundColor = .white
     }
 }
